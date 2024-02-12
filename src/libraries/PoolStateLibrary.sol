222ccd60236e5ea4bdca691b96cac1b4db15f2e9
saucepoint <saucepoint@protonmail.com>
2024-02-12 14:39:28 -0500

    documentation and remove magic number offsets

--- src/libraries/PoolStateLibrary.sol
+++ src/libraries/PoolStateLibrary.sol
@@ -12,11 +12,29 @@ library PoolStateLibrary {
     // | pools                 | mapping(PoolId => struct Pool.State)                                | 8    | 0      | 32    | lib/v4-core/src/PoolManager.sol:PoolManager |
     uint256 public constant POOLS_SLOT = 8;
 
+    // index of feeGrowthGlobal0X128 in Pool.State
+    uint256 public constant FEE_GROWTH_GLOBAL0_OFFSET = 1;
+    // index of feeGrowthGlobal1X128 in Pool.State
+    uint256 public constant FEE_GROWTH_GLOBAL1_OFFSET = 2;
+
+    // index of liquidity in Pool.State
+    uint256 public constant LIQUIDITY_OFFSET = 3;
+
+    // index of TicksInfo mapping in Pool.State
+    uint256 public constant TICK_INFO_OFFSET = 4;
+
+    // index of tickBitmap mapping in Pool.State
+    uint256 public constant TICK_BITMAP_OFFSET = 5;
+
+    // index of Position.Info mapping in Pool.State
+    uint256 public constant POSITION_INFO_OFFSET = 6;
+
     /**
-     * @dev Retrieves the slot 0 information of a pool.
+     * @notice Get Slot0 of the pool: sqrtPriceX96, tick, protocolFee, swapFee
+     * @dev Corresponds to pools[poolId].slot0
      * @param manager The pool manager contract.
      * @param poolId The ID of the pool.
-     * @return sqrtPriceX96 The square root of the price of the pool.
+     * @return sqrtPriceX96 The square root of the price of the pool, in Q96 precision.
      * @return tick The current tick of the pool.
      * @return protocolFee The protocol fee of the pool.
      * @return swapFee The swap fee of the pool.
@@ -26,7 +44,7 @@ library PoolStateLibrary {
         view
         returns (uint160 sqrtPriceX96, int24 tick, uint16 protocolFee, uint24 swapFee)
     {
-        // value slot of poolId key: `pools[poolId]`
+        // slot key of Pool.State value: `pools[poolId]`
         bytes32 stateSlot = keccak256(abi.encodePacked(PoolId.unwrap(poolId), bytes32(POOLS_SLOT)));
 
         bytes32 data = manager.extsload(stateSlot);
@@ -47,14 +65,15 @@ library PoolStateLibrary {
     }
 
     /**
-     * @dev Retrieves the tick information of a pool at a specific tick.
+     * @notice Retrieves the tick information of a pool at a specific tick.
+     * @dev Corresponds to pools[poolId].ticks[tick]
      * @param manager The pool manager contract.
      * @param poolId The ID of the pool.
      * @param tick The tick to retrieve information for.
-     * @return liquidityGross The gross liquidity at the tick.
-     * @return liquidityNet The net liquidity at the tick.
-     * @return feeGrowthOutside0X128 The fee growth outside the tick for token0.
-     * @return feeGrowthOutside1X128 The fee growth outside the tick for token1.
+     * @return liquidityGross The total position liquidity that references this tick
+     * @return liquidityNet The amount of net liquidity added (subtracted) when tick is crossed from left to right (right to left)
+     * @return feeGrowthOutside0X128 fee growth per unit of liquidity on the _other_ side of this tick (relative to the current tick)
+     * @return feeGrowthOutside1X128 fee growth per unit of liquidity on the _other_ side of this tick (relative to the current tick)
      */
     function getTickInfo(IPoolManager manager, PoolId poolId, int24 tick)
         internal
@@ -66,13 +85,13 @@ library PoolStateLibrary {
             uint256 feeGrowthOutside1X128
         )
     {
-        // value slot of poolId key: `pools[poolId]`
+        // slot key of Pool.State value: `pools[poolId]`
         bytes32 stateSlot = keccak256(abi.encodePacked(PoolId.unwrap(poolId), bytes32(POOLS_SLOT)));
 
-        // reads 5th word of Pool.State, `mapping(int24 => TickInfo) ticks`
-        bytes32 ticksMapping = bytes32(uint256(stateSlot) + uint256(4));
+        // Pool.State: `mapping(int24 => TickInfo) ticks`
+        bytes32 ticksMapping = bytes32(uint256(stateSlot) + TICK_INFO_OFFSET);
 
-        // value slot of the tick key: `pools[poolId].ticks[tick]
+        // slot key of the tick key: `pools[poolId].ticks[tick]
         bytes32 slot = keccak256(abi.encodePacked(int256(tick), ticksMapping));
 
         // read all 3 words of the TickInfo struct
@@ -86,25 +105,26 @@ library PoolStateLibrary {
     }
 
     /**
-     * @dev Retrieves the liquidity information of a pool at a specific tick.
+     * @notice Retrieves the liquidity information of a pool at a specific tick.
+     * @dev Corresponds to pools[poolId].ticks[tick].liquidityGross and pools[poolId].ticks[tick].liquidityNet. A more gas efficient version of getTickInfo
      * @param manager The pool manager contract.
      * @param poolId The ID of the pool.
      * @param tick The tick to retrieve liquidity for.
-     * @return liquidityGross The gross liquidity at the tick.
-     * @return liquidityNet The net liquidity at the tick.
+     * @return liquidityGross The total position liquidity that references this tick
+     * @return liquidityNet The amount of net liquidity added (subtracted) when tick is crossed from left to right (right to left)
      */
     function getTickLiquidity(IPoolManager manager, PoolId poolId, int24 tick)
         internal
         view
         returns (uint128 liquidityGross, int128 liquidityNet)
     {
-        // value slot of poolId key: `pools[poolId]`
+        // slot key of Pool.State value: `pools[poolId]`
         bytes32 stateSlot = keccak256(abi.encodePacked(PoolId.unwrap(poolId), bytes32(POOLS_SLOT)));
 
-        // reads 5th word of Pool.State, `mapping(int24 => TickInfo) ticks`
-        bytes32 ticksMapping = bytes32(uint256(stateSlot) + uint256(4));
+        // Pool.State: `mapping(int24 => TickInfo) ticks`
+        bytes32 ticksMapping = bytes32(uint256(stateSlot) + TICK_INFO_OFFSET);
 
-        // value slot of the tick key: `pools[poolId].ticks[tick]
+        // slot key of the tick key: `pools[poolId].ticks[tick]
         bytes32 slot = keccak256(abi.encodePacked(int256(tick), ticksMapping));
 
         bytes32 value = manager.extsload(slot);
@@ -115,27 +135,29 @@ library PoolStateLibrary {
     }
 
     /**
-     * @dev Retrieves the fee growth outside a tick range of a pool.
+     * @notice Retrieves the fee growth outside a tick range of a pool
+     * @dev Corresponds to pools[poolId].ticks[tick].feeGrowthOutside0X128 and pools[poolId].ticks[tick].feeGrowthOutside1X128. A more gas efficient version of getTickInfo
      * @param manager The pool manager contract.
      * @param poolId The ID of the pool.
      * @param tick The tick to retrieve fee growth for.
-     * @return feeGrowthOutside0X128 The fee growth outside the tick range for token0.
-     * @return feeGrowthOutside1X128 The fee growth outside the tick range for token1.
+     * @return feeGrowthOutside0X128 fee growth per unit of liquidity on the _other_ side of this tick (relative to the current tick)
+     * @return feeGrowthOutside1X128 fee growth per unit of liquidity on the _other_ side of this tick (relative to the current tick)
      */
     function getTickFeeGrowthOutside(IPoolManager manager, PoolId poolId, int24 tick)
         internal
         view
         returns (uint256 feeGrowthOutside0X128, uint256 feeGrowthOutside1X128)
     {
-        // value slot of poolId key: `pools[poolId]`
+        // slot key of Pool.State value: `pools[poolId]`
         bytes32 stateSlot = keccak256(abi.encodePacked(PoolId.unwrap(poolId), bytes32(POOLS_SLOT)));
 
-        // reads 5th word of Pool.State, `mapping(int24 => TickInfo) ticks`
-        bytes32 ticksMapping = bytes32(uint256(stateSlot) + uint256(4));
+        // Pool.State: `mapping(int24 => TickInfo) ticks`
+        bytes32 ticksMapping = bytes32(uint256(stateSlot) + TICK_INFO_OFFSET);
 
-        // value slot of the tick key: `pools[poolId].ticks[tick]
+        // slot key of the tick key: `pools[poolId].ticks[tick]
         bytes32 slot = keccak256(abi.encodePacked(int256(tick), ticksMapping));
 
+        // TODO: offset to feeGrowth, to avoid 3-word read
         bytes memory data = manager.extsload(slot, 3);
         assembly {
             feeGrowthOutside0X128 := mload(add(data, 64))
@@ -144,7 +166,8 @@ library PoolStateLibrary {
     }
 
     /**
-     * @dev Retrieves the global fee growth of a pool.
+     * @notice Retrieves the global fee growth of a pool.
+     * @dev Corresponds to pools[poolId].feeGrowthGlobal0X128 and pools[poolId].feeGrowthGlobal1X128
      * @param manager The pool manager contract.
      * @param poolId The ID of the pool.
      * @return feeGrowthGlobal0 The global fee growth for token0.
@@ -155,37 +178,46 @@ library PoolStateLibrary {
         view
         returns (uint256 feeGrowthGlobal0, uint256 feeGrowthGlobal1)
     {
-        // value slot of poolId key: `pools[poolId]`
+        // slot key of Pool.State value: `pools[poolId]`
         bytes32 stateSlot = keccak256(abi.encodePacked(PoolId.unwrap(poolId), bytes32(POOLS_SLOT)));
 
-        // reads 2nd word of Pool.State, `uint256 feeGrowthGlobal0X128`
-        bytes32 slot_feeGrowthGlobal0X128 = bytes32(uint256(stateSlot) + uint256(1));
+        // Pool.State, `uint256 feeGrowthGlobal0X128`
+        bytes32 slot_feeGrowthGlobal0X128 = bytes32(uint256(stateSlot) + FEE_GROWTH_GLOBAL0_OFFSET);
 
         // reads 3rd word of Pool.State, `uint256 feeGrowthGlobal1X128`
-        bytes32 slot_feeGrowthGlobal1X128 = bytes32(uint256(stateSlot) + uint256(2));
+        // bytes32 slot_feeGrowthGlobal1X128 = bytes32(uint256(stateSlot) + uint256(FEE_GROWTH_GLOBAL1_OFFSET));
 
-        feeGrowthGlobal0 = uint256(manager.extsload(slot_feeGrowthGlobal0X128));
-        feeGrowthGlobal1 = uint256(manager.extsload(slot_feeGrowthGlobal1X128));
+        // feeGrowthGlobal0 = uint256(manager.extsload(slot_feeGrowthGlobal0X128));
+        // feeGrowthGlobal1 = uint256(manager.extsload(slot_feeGrowthGlobal1X128));
+
+        // read the 2 words of feeGrowthGlobal
+        bytes memory data = manager.extsload(slot_feeGrowthGlobal0X128, 2);
+        assembly {
+            feeGrowthGlobal0 := mload(add(data, 32))
+            feeGrowthGlobal1 := mload(add(data, 64))
+        }
     }
 
     /**
-     * @dev Retrieves the liquidity of a pool.
+     * @notice Retrieves total the liquidity of a pool.
+     * @dev Corresponds to pools[poolId].liquidity
      * @param manager The pool manager contract.
      * @param poolId The ID of the pool.
      * @return liquidity The liquidity of the pool.
      */
     function getLiquidity(IPoolManager manager, PoolId poolId) internal view returns (uint128 liquidity) {
-        // value slot of poolId key: `pools[poolId]`
+        // slot key of Pool.State value: `pools[poolId]`
         bytes32 stateSlot = keccak256(abi.encodePacked(PoolId.unwrap(poolId), bytes32(POOLS_SLOT)));
 
-        // reads 4th word of Pool.State, `uint128 liquidity`
-        bytes32 slot = bytes32(uint256(stateSlot) + uint256(3));
+        // Pool.State: `uint128 liquidity`
+        bytes32 slot = bytes32(uint256(stateSlot) + LIQUIDITY_OFFSET);
 
         liquidity = uint128(uint256(manager.extsload(slot)));
     }
 
     /**
-     * @dev Retrieves the tick bitmap of a pool at a specific tick.
+     * @notice Retrieves the tick bitmap of a pool at a specific tick.
+     * @dev Corresponds to pools[poolId].tickBitmap[tick]
      * @param manager The pool manager contract.
      * @param poolId The ID of the pool.
      * @param tick The tick to retrieve the bitmap for.
@@ -196,20 +228,21 @@ library PoolStateLibrary {
         view
         returns (uint256 tickBitmap)
     {
-        // value slot of poolId key: `pools[poolId]`
+        // slot key of Pool.State value: `pools[poolId]`
         bytes32 stateSlot = keccak256(abi.encodePacked(PoolId.unwrap(poolId), bytes32(POOLS_SLOT)));
 
-        // reads 6th word of Pool.State, `mapping(int16 => uint256) tickBitmap;`
-        bytes32 tickBitmapMapping = bytes32(uint256(stateSlot) + uint256(5));
+        // Pool.State: `mapping(int16 => uint256) tickBitmap;`
+        bytes32 tickBitmapMapping = bytes32(uint256(stateSlot) + TICK_BITMAP_OFFSET);
 
-        // value slot of the mapping key: `pools[poolId].tickBitmap[tick]
+        // slot id of the mapping key: `pools[poolId].tickBitmap[tick]
         bytes32 slot = keccak256(abi.encodePacked(int256(tick), tickBitmapMapping));
 
         tickBitmap = uint256(manager.extsload(slot));
     }
 
     /**
-     * @dev Retrieves the position information of a pool at a specific position ID.
+     * @notice Retrieves the position information of a pool at a specific position ID.
+     * @dev Corresponds to pools[poolId].positions[positionId]
      * @param manager The pool manager contract.
      * @param poolId The ID of the pool.
      * @param positionId The ID of the position.
@@ -222,11 +255,11 @@ library PoolStateLibrary {
         view
         returns (uint128 liquidity, uint256 feeGrowthInside0LastX128, uint256 feeGrowthInside1LastX128)
     {
-        // value slot of poolId key: `pools[poolId]`
+        // slot key of Pool.State value: `pools[poolId]`
         bytes32 stateSlot = keccak256(abi.encodePacked(PoolId.unwrap(poolId), bytes32(POOLS_SLOT)));
 
-        // reads 7th word of Pool.State, `mapping(bytes32 => Position.Info) positions;`
-        bytes32 positionMapping = bytes32(uint256(stateSlot) + uint256(6));
+        // Pool.State: `mapping(bytes32 => Position.Info) positions;`
+        bytes32 positionMapping = bytes32(uint256(stateSlot) + POSITION_INFO_OFFSET);
 
         // first value slot of the mapping key: `pools[poolId].positions[positionId] (liquidity)
         bytes32 slot = keccak256(abi.encodePacked(positionId, positionMapping));
@@ -242,7 +275,8 @@ library PoolStateLibrary {
     }
 
     /**
-     * @dev Calculates the fee growth inside a tick range of a pool.
+     * @notice Live calculate the fee growth inside a tick range of a pool
+     * @dev pools[poolId].feeGrowthInside0LastX128 in Position.Info is cached and can become stale. This function will live calculate the feeGrowthInside
      * @param manager The pool manager contract.
      * @param poolId The ID of the pool.
      * @param tickLower The lower tick of the range.
